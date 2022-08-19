1ff02c95a5007ab0f7e872ad27f7bd6586e11d20
SWE-Ahmed <master.necro15@gmail.com>
2022-08-19 18:54:37 +0300

    add: targets.config + modified code struct

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 ## import the required libraries
-from pyrogram import Client
+from pyrogram import Client, filters
+import tensorflow as tf
 
-## define helper functions
-
+## helper function for getting credentials
 # read in the credentials
 def get_credentials():
     """
@@ -18,20 +18,41 @@ def get_credentials():
     
     return (api_id, api_hash, bot_token)
 
-# MAIN function
-async def main():
-    async with app:
-        await app.send_message("me", "Hi!")
-
-#############################################
-
-# initialize the telegram bot
+# initialize the telegram bot and load in the model
 # these variables can later be omitted if the sessions are created for both the user and the bot
 api_id, api_hash, bot_token = get_credentials()
 app = Client("my_account", 
              api_id=api_id, 
              api_hash=api_hash)
+model = tf.keras.models.load_model("../model/spam_detection_model")
 
+#################################################
+
+## handlers for the bot
+
+# monitor the messages sent
+@app.on_message(filters.text)
+async def monitor(client, message):
+    result = model.predict([message.text])
+    result = tf.squeeze(tf.round(result))
+    
+    if result == 1:
+        await message.reply('SPAM DETECTED')
+
+# @app.on_message(filters.text)
+# def text_handler(client, message):
+#     if message.text.lower() == "hi":
+#         message.reply("Hello")
+#     elif message.text.lower() == "profile":
+#         userId = message.reply_to_message.from_user.id
+#         profile = app.get_users(userId)
+#         id_ = profile.id
+#         name = profile.first_name + ' ' + profile.last_name
+#         userName = profile.username
+#         message.reply(f'Name: {name}\n ID: {id_}\n Username: {userName}')
+
+
+#################################################
 
 #! app starting point
-app.run(main())
+app.run()
