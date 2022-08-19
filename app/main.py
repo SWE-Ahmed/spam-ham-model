244bf8a4c4c684910b5ce31daa6408410f710387
SWE-Ahmed <master.necro15@gmail.com>
2022-08-19 22:54:22 +0300

    add: ML implementation

--- app/main.py
+++ app/main.py
@@ -18,38 +18,44 @@ def get_credentials():
     
     return (api_id, api_hash, bot_token)
 
+#################################################
+
 # initialize the telegram bot and load in the model
 # these variables can later be omitted if the sessions are created for both the user and the bot
 api_id, api_hash, bot_token = get_credentials()
-app = Client("my_account", 
+app = Client("my_bot", 
              api_id=api_id, 
-             api_hash=api_hash)
+             api_hash=api_hash,
+             bot_token=bot_token)
+
+print('>>> Loading Model...')
 model = tf.keras.models.load_model("../model/spam_detection_model")
+print('>>> Model Loaded Successfully.')
 
 #################################################
 
 ## handlers for the bot
-
 # monitor the messages sent
 @app.on_message(filters.text)
 async def monitor(client, message):
-    result = model.predict([message.text])
-    result = tf.squeeze(tf.round(result))
     
-    if result == 1:
-        await message.reply('SPAM DETECTED')
-
-# @app.on_message(filters.text)
-# def text_handler(client, message):
-#     if message.text.lower() == "hi":
-#         message.reply("Hello")
-#     elif message.text.lower() == "profile":
-#         userId = message.reply_to_message.from_user.id
-#         profile = app.get_users(userId)
-#         id_ = profile.id
-#         name = profile.first_name + ' ' + profile.last_name
-#         userName = profile.username
-#         message.reply(f'Name: {name}\n ID: {id_}\n Username: {userName}')
+    # model classifies commands as spam, thus we ignore them
+    if (message.text).split()[0][0] != '/':
+        # classify the incoming message as either spam or not
+        result = model.predict([message.text])
+        result = tf.squeeze(tf.round(result))
+        if result == 1:
+            name = message.from_user.first_name + ' ' + message.from_user.last_name
+            chat_id = message.chat.id
+            id = message.from_user.id
+            await message.delete()
+            await client.ban_chat_member(chat_id=chat_id, user_id=id)
+            await client.send_message(text=f"User, {name}, with ID, {id}, has been kicked out for sending spam.", chat_id=chat_id)
+        
+# commands for the bot
+@app.on_message(filters.command(['start']))
+async def commands_handler(client, message):
+    message.reply('Online and working at full capacity...')
 
 
 #################################################
